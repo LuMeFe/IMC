17657112227cf30212743a61572d64a135e2c03f
Lucas Menezes Fernandes <167871277+LuMeFe@users.noreply.github.com>
2024-08-26 21:17:41 -0300

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { HandlebarsAdapter } from '@nestjs/handlebars';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('hbs', new HandlebarsAdapter());
+  app.setViewEngine('hbs');
+
+  await app.listen(3000);
+}
+bootstrap();
